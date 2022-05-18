3864e71e91d3c01efe07019959e807ff8e1288c8
Josue-Luna <josueluna650@gmail.com>
2022-05-18 16:48:27 -0300

    b

--- funciones.js
+++ funciones.js
@@ -9,14 +9,26 @@ function colocar_img(){
 	ctx.drawImage(img,5,5);
 	
 }
+function colocar_img2(){
+	var can = document.getElementById("micanvas");
+	var ctx = can.getContext("2d");
+	var img = document.getElementById("verde");
+	ctx.font = "30px Arial";
+	ctx.fillStyle ="red";
+	ctx.fillText("Hola",15,15);
+	
+	ctx.drawImage(img,5,5);
+	
+}
 function guardar_canvas_local(){
 	var can = document.getElementById("micanvas");
 	var ctx = can.getContext("2d");
-	var imagen = can.toDataURL();
-	var link = document.createElement("a");
-	link.href = imagen;
-	link.download = "saveimgcanvas";
-	document.body.appendChild(link);
-	link.click();
-	document.body.removeChild(link);
+	var imagen = can.toDataURL("image/jpeg").replace("image/jpeg","image/octet-stream");
+	//var link = document.createElement("a");
+	//link.href = imagen;
+	//link.download = "saveimgcanvas";
+	//document.body.appendChild(link);
+	//link.click();
+	//document.body.removeChild(link);
+	window.location.href = imagen;
 }
