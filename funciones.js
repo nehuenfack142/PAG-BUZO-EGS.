6554c53699d6709f15f1ed4794a57061a7e33f6f
Josue-Luna <josueluna650@gmail.com>
2022-05-18 16:08:18 -0300

    intento

--- funciones.js
+++ funciones.js
@@ -2,8 +2,11 @@ function colocar_img(){
 	var can = document.getElementById("micanvas");
 	var ctx = can.getContext("2d");
 	var img = document.getElementById("imagen");
+	ctx.font = "30px Arial";
+	ctx.fillStyle ="red";
+	ctx.fillText("Hola",5,5);
 	
-	ctx.drawImage(img,50,50);
+	ctx.drawImage(img,5,5);
 	
 }
 function guardar_canvas_local(){
